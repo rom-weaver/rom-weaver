bcd044cb05812411732dffa528380636fce215bc
brandonocasey <brandonocasey@gmail.com>
2026-05-22 07:01:00 -0400

    chore(vendor): remove stale sevenz snapshot

--- vendor/sevenz-rust2-0.21.0/src/util.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(all(feature = "compress", feature = "util", not(target_arch = "wasm32")))]
-pub(crate) mod compress;
-#[cfg(all(feature = "util", not(target_arch = "wasm32")))]
-pub(crate) mod decompress;
-
-#[cfg(target_arch = "wasm32")]
-pub(crate) mod wasm;
